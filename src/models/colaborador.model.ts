528c1e1fd01dc9b230cf89900a6b022dbac8b11b
luis-nav <luiscatodd@gmail.com>
2024-02-27 13:46:10 -0600

    fix: colaborador model naming and imports

--- src/models/colaborador.model.ts
+++ src/models/colaborador.model.ts
@@ -1,4 +1,4 @@
-import mongoose, { Model, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { compare, genSalt, hash } from "bcrypt";
 
 import { Colaborador } from "../interfaces/colaborador.interface";
