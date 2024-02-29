9b461307a49a3dc9a5f69e63a3c399e888d9229b
Luis-Nav <luiscatodd@gmail.com>
2024-02-29 12:50:16 -0600

    feat: db connection on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
+import mongoose from "mongoose";
 import app from "./app";
 
 const port:number = 3000;
+const uri: string = "mongodb+srv://todd:webdev24@pokedexcluster.6rsbbhn.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-})
+const main = async () => {
+    await mongoose.connect(uri)
+        .then(() => console.log("[server]: Database connected"));
+
+    app.listen(port, () =>
+        console.log(`[server]: Server is running at http://localhost:${port}`))
+};
+
+main();
