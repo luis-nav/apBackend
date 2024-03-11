4571267f80797af4b636c8d446472b90a4858e15
Luis-Nav <luiscatodd@gmail.com>
2024-03-11 11:02:35 -0600

    add: reunion model

---
+++ src/models/reunion.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+import { Reunion } from "../interfaces/reunion.interface";
+
+const reunionSchema = new Schema<Reunion>({
+    fecha: {
+        type: Date,
+        required: true,
+    },
+    temaReunion: {
+        type: String,
+        required: true,
+    },
+    medioReunion: {
+        type: String,
+        required: true,
+    },
+    formatoInvitacion: {
+        type: String,
+        required: true,
+    },
+    colaboradores: {
+        type: [Schema.Types.ObjectId],
+        ref: "Colaborador",
+    }
+});
+
+export const ReunionModel = mongoose.model<Reunion>("Reunion", reunionSchema);
