46d518d89c365756ca144e4db44443ab828eb6ad
luis-nav <luiscatodd@gmail.com>
2024-03-12 14:57:15 -0600

    add: Proyecto Controller Inicial

---
+++ src/controllers/proyecto.controller.ts
@@ -0,0 +1,43 @@
+import { Request, Response, RequestHandler } from "express";
+
+import { ProyectoModel } from "../models/proyecto.model";
+import { EstadoTareaModel } from '../models/estadoTarea.model';
+import { ColaboradorModel } from "../models/colaborador.model";
+
+
+export const getProyectos: RequestHandler = async (req: Request, res: Response) => {
+    const proyectos = await ProyectoModel.find({})
+        .populate("responsable")
+        .populate("estado")
+        .lean().exec();
+    return res.status(200).json(proyectos);
+}
+
+export const getProyecto: RequestHandler = async (req: Request, res: Response) => {
+    const { nombre } = req.body;
+    // findOne({ nombre: nombre }) <- En notacion de JS es lo mismo que hacer esto
+    const proyecto = await ProyectoModel.findOne({ nombre })
+        .populate("responsable")
+        .populate("estado")
+        .populate({ path: "foro", populate: { path: "mensajes", populate: { path: "colaborador" } } })
+        .populate({ path: "reuniones", populate: { path: "colaboradores" } })
+        .lean().exec();
+    return res.status(200).json(proyecto);
+}
+
+export const crearProyecto: RequestHandler = async (req: Request, res: Response) => {
+    const { nombre, presupuesto, descripcion, fechaInicio, nombreResponsable } = req.body;
+    const estado = await EstadoTareaModel.findOne({ nombre: "Iniciando" });
+    const responsable = await ColaboradorModel.findOne({ nombre: nombreResponsable });
+    const proyecto = new ProyectoModel({
+        nombre, 
+        presupuesto, 
+        descripcion,
+        estado,
+        responsable,
+        fechaInicio
+    });
+    await proyecto.save();
+
+    return res.status(201).json({ message: "Proyecto creado!"})
+}
--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import departamentoRouter from "./departamento.routes"
 import colaboradorRouter from "./colaborador.routes"
+import proyectoRouter from "./proyecto.routes"
 
 const router = Router();
 
 router.use("/departamentos", departamentoRouter)
 router.use("/colaboradores", colaboradorRouter)
+router.use("/proyectos", proyectoRouter)
 
 export default router;
