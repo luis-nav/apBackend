ee378be8a6d54fe992002a6a2cf0414d6c1af5e6
Luis-Nav <luiscatodd@gmail.com>
2024-04-10 15:21:44 -0600

    fix: populate estado tarea in recursos controller

--- src/controllers/recurso.controller.ts
+++ src/controllers/recurso.controller.ts
@@ -13,7 +13,7 @@ export const addRecurso: RequestHandler = async (req: Request, res: Response) =>
     const nombreProyecto = req.params.nombreProyecto;
     const recurso = req.body;
     try {
-        const proyecto = await ProyectoModel.findOneAndUpdate({ nombre: nombreProyecto }, { $push: { recursos: recurso }}).populate("tareas.responsable", ["nombre"]).populate("tareas.estado");
+        const proyecto = await ProyectoModel.findOneAndUpdate({ nombre: nombreProyecto }, { $push: { recursos: recurso }}).populate("tareas.responsable", ["nombre"]);
         if (!proyecto) return res.status(400).json({ message: "Error: Project not found "});
         return res.status(201).json({ message: "Resource created!", proyecto});  
     } catch (error) {
@@ -25,7 +25,7 @@ export const removeRecurso: RequestHandler = async (req: Request, res: Response)
     const nombreProyecto = req.params.nombreProyecto;
     const { nombre } = req.body
     try {
-        const proyecto = await ProyectoModel.findOneAndUpdate({ nombre: nombreProyecto }, { $pull: { recursos: { nombre } }}).populate("tareas.responsable", ["nombre"]).populate("tareas.estado");
+        const proyecto = await ProyectoModel.findOneAndUpdate({ nombre: nombreProyecto }, { $pull: { recursos: { nombre } }}).populate("tareas.responsable", ["nombre"]);
         if (!proyecto) return res.status(400).json({ message: "Error: Failed to delete project resource"});
         return res.status(201).json({ message: "Resource deleted!", proyecto});  
     } catch (error) {
