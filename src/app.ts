5bfb5967c189c0b118af2219634125099c699eac
Luis-Nav <luiscatodd@gmail.com>
2024-05-16 20:16:49 -0600

    Add: CORS config and refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
+// Express
 import express, { Express, Request, Response } from "express";
 
+// Cross Origin Policy
+import cors from "cors";
+import { corsOptions } from "./cors.config";
+
+// API Router
 import apiRouter from "./routes/api.routes"
 
 const app: Express = express();
 
 app.use(express.json());
 
-app.use("/api", apiRouter)
+apiRouter.use(cors(corsOptions));
+
+app.use("/api", apiRouter);
 
 app.get("*", (req: Request, res:Response) => {
     return res.status(404).json({ message: "The route does not exist"});
---
+++ src/cors.config.ts
@@ -0,0 +1,6 @@
+import { CorsOptions } from "cors";
+
+export const corsOptions: CorsOptions = {
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+};
